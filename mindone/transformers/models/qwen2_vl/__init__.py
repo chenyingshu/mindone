36172f10171060116be344e30f51d5a376f505db
Yingshu Chen <yingshu2008@gmail.com>
2024-10-04 18:17:54 +0800

    qwen2-vl model init

---
+++ mindone/transformers/models/qwen2_vl/__init__.py
@@ -0,0 +1,8 @@
+from .configuration_qwen2_vl import Qwen2VLConfig
+from .processing_qwen2_vl import Qwen2VLProcessor
+from .modeling_qwen2_vl import (
+    Qwen2VLForConditionalGeneration,
+    Qwen2VLModel,
+    Qwen2VLPreTrainedModel,
+)
+from .image_processing_qwen2_vl import Qwen2VLImageProcessor
